fc0fa9b4dfafe47fcd1c8b8e659085d34d58bbfe
leirensheng <leirensheng@163.com>
2023-05-13 17:27:35 +0800

    feat: update

---
+++ pages/damai/ticket.vue
@@ -0,0 +1,39 @@
+<template>
+    <iframe id="iframe_id" :src="src" frameborder="0" style="width:100%;" height="100%" ></iframe>
+
+</template>
+
+<script>
+import RemoteConfig from '../../components/remote.vue'
+export default {
+    components:{
+        RemoteConfig
+    },
+    data() {
+        return {
+            src:"https://m.damai.cn/damai/activity/broadlist/index.html?city=852"
+        };
+    },
+    created() {
+
+    },
+    async onPullDownRefresh() {
+        this.src = ''
+        setTimeout(()=>{
+            this.src="https://m.damai.cn/damai/activity/broadlist/index.html"
+            uni.stopPullDownRefresh();
+        },0)
+        // await this.$refs.remote.getConfig()
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
